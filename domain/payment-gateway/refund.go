fcc8a335510e850d1af78c62601f822dcedd24f9
tambarie <gt.gbaragbo@gmail.com>
2022-03-16 21:30:19 +0100

    add transactionID to the struct

--- domain/payment-gateway/refund.go
+++ domain/payment-gateway/refund.go
@@ -1,6 +1,7 @@
 package domain
 
 type Refund struct {
+	TransactionID   string  `json:"transaction_id" bson:"transaction_id"`
 	AuthorizationID string  `json:"authorization_id" bson:"authorization_id"`
 	Amount          float64 `json:"amount" bson:"amount"`
 }
