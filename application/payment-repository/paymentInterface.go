e54f4304f8482ade4a98a10c3db61864ebbaaa0a
tambarie <gt.gbaragbo@gmail.com>
2022-03-16 16:51:33 +0100

    add three methods to Repository

--- application/payment-repository/paymentInterface.go
+++ application/payment-repository/paymentInterface.go
@@ -1,8 +1,15 @@
 package payment_repository
 
-import domain "github.com/Tambarie/payment-gateway/domain/payment-gateway"
+import (
+	domain "github.com/Tambarie/payment-gateway/domain/payment-gateway"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 // Repository interface
 type Repository interface {
 	CreateMerchant(card *domain.Card) (*domain.Card, error)
+	GetID(id string) (bson.M, error)
+	UpdateAccount(amount float64, id string) (*mongo.UpdateResult, error)
+	SaveCapturedTransaction(capture *domain.Capture) (*domain.Capture, error)
 }
