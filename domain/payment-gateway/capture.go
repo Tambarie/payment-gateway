ad449efa67f297f6b621ca53104c5fd6e7222db1
tambarie <gt.gbaragbo@gmail.com>
2022-03-16 21:27:49 +0100

    added  transaction ID

--- domain/payment-gateway/capture.go
+++ domain/payment-gateway/capture.go
@@ -1,6 +1,7 @@
 package domain
 
 type Capture struct {
+	TransactionID   string  `json:"transaction_id" bson:"transaction_id"`
 	AuthorizationID string  `json:"authorization_id" bson:"authorization_id"`
 	Amount          float64 `json:"amount" bson:"amount"`
 }
