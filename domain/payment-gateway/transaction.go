1882192e3e2d8a510519eab620e81c060ff57dd9
tambarie <gt.gbaragbo@gmail.com>
2022-03-17 13:27:58 +0100

    change capture struct to transaction

--- domain/payment-gateway/transaction.go
+++ domain/payment-gateway/transaction.go
@@ -1,6 +1,6 @@
 package domain
 
-type Capture struct {
+type Transaction struct {
 	TransactionID   string  `json:"transaction_id,omitempty"  bson:"transaction_id"`
 	AuthorizationID string  `json:"authorization_id" bson:"authorization_id"`
 	Amount          float64 `json:"amount" bson:"amount"`
