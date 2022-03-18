6b18c5b12c0aa3a4d05498bc061b7eca7aeeb9de
tambarie <gt.gbaragbo@gmail.com>
2022-03-18 19:58:38 +0100

    modified user struct

--- domain/payment-gateway/user.go
+++ domain/payment-gateway/user.go
@@ -1,11 +1,11 @@
 package domain
 
 type User struct {
-	Reference    string `json:"id,omitempty" bson:"id"`
-	FirstName    string `json:"first_name,omitempty" bson:"first_name"`
-	LastName     string `json:"last_name,omitempty" bson:"last_name"`
-	Email        string `json:"email,omitempty" bson:"email"`
+	Reference    string `json:"reference" bson:"reference"`
+	FirstName    string `json:"first_name" bson:"first_name"`
+	LastName     string `json:"last_name" bson:"last_name"`
+	Email        string `json:"email" bson:"email"`
 	Password     string `json:"-" bson:"password"`
 	HashPassword []byte `json:"-" bson:"hash_password"`
-	TimeCreated  string `json:"-" bson:"time_created"`
+	TimeCreated  string `json:"time-created" bson:"time_created"`
 }
