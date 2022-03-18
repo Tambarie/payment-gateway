a515448e9d42eb30d0b7235a7e639ae8557133a7
tambarie <gt.gbaragbo@gmail.com>
2022-03-18 04:15:04 +0100

    initialized log

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/Tambarie/payment-gateway/application/server"
+	"github.com/Tambarie/payment-gateway/domain/helpers"
 	"github.com/joho/godotenv"
 	"log"
 )
 
 func main() {
+	helpers.InitializeLogDir()
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Error loading .env with godotenv: %s", err)
