829466433910aa426884c128dce06591f9ebe7ed
tambarie <gt.gbaragbo@gmail.com>
2022-03-18 04:16:11 +0100

    add logs to db methods

--- application/payment-repository/paymentRepository.go
+++ application/payment-repository/paymentRepository.go
@@ -2,6 +2,7 @@ package payment_repository
 
 import (
 	"fmt"
+	"github.com/Tambarie/payment-gateway/domain/helpers"
 	domain "github.com/Tambarie/payment-gateway/domain/payment-gateway"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -23,6 +24,9 @@ func NewPaymentGatewayRepositoryDB(client *mongo.Client) *RepositoryDB {
 }
 
 func (paymentRepo *RepositoryDB) CreateMerchant(card *domain.Card) (*domain.Card, error) {
+
+	helpers.LogEvent("INFO", fmt.Sprintf("Creating Merchant with reference: %s ...", card))
+
 	collection := paymentRepo.db.Database("payment-gateway").Collection("gateway")
 	result, err := collection.InsertOne(context.TODO(), card)
 	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
@@ -30,6 +34,7 @@ func (paymentRepo *RepositoryDB) CreateMerchant(card *domain.Card) (*domain.Card
 }
 
 func (paymentRepo *RepositoryDB) GetCardByID(id string) (bson.M, error) {
+	helpers.LogEvent("INFO", fmt.Sprintf("Getting card by ID  with id: %s ...", id))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("gateway")
 
 	var result bson.M
@@ -48,7 +53,7 @@ func (paymentRepo *RepositoryDB) GetCardByID(id string) (bson.M, error) {
 }
 
 func (paymentRepo *RepositoryDB) UpdateAccount(amount float64, id string) (interface{}, error) {
-
+	helpers.LogEvent("INFO", fmt.Sprintf("Updating account with amount: %v and id :%s", amount, id))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("gateway")
 
 	filter := bson.D{{"id", id}}
@@ -71,6 +76,7 @@ func (paymentRepo *RepositoryDB) UpdateAccount(amount float64, id string) (inter
 }
 
 func (paymentRepo *RepositoryDB) SaveCapturedTransaction(capture *domain.Transaction) (*mongo.InsertOneResult, error) {
+	helpers.LogEvent("INFO", fmt.Sprintf("Saving Captured transaction with reference: %s ...", capture))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("captured-transaction")
 	result, err := collection.InsertOne(context.TODO(), capture)
 	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
@@ -78,6 +84,7 @@ func (paymentRepo *RepositoryDB) SaveCapturedTransaction(capture *domain.Transac
 }
 
 func (paymentRepo *RepositoryDB) GetCapturedTransactionByTransactionID(id string) (*domain.Transaction, error) {
+	helpers.LogEvent("INFO", fmt.Sprintf("Getting Captured transaction by transaction ID :%s", id))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("captured-transaction")
 	var result *domain.Transaction
 	err := collection.FindOne(
@@ -95,6 +102,7 @@ func (paymentRepo *RepositoryDB) GetCapturedTransactionByTransactionID(id string
 }
 
 func (paymentRepo *RepositoryDB) GetCapturedTransactionByAuthorizationID(id string) (bson.M, error) {
+	helpers.LogEvent("INFO", fmt.Sprintf("Getting Captured transaction by authorization ID :%s", id))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("captured-transaction")
 	var result bson.M
 	err := collection.FindOne(
@@ -112,7 +120,7 @@ func (paymentRepo *RepositoryDB) GetCapturedTransactionByAuthorizationID(id stri
 }
 
 func (paymentRepo *RepositoryDB) RefundUpdateAccount(amount float64, id string, count int) (interface{}, error) {
-
+	helpers.LogEvent("INFO", fmt.Sprintf("Updating account with :%v and id :%s ", amount, id))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("gateway")
 
 	filter := bson.D{{"id", id}}
@@ -135,6 +143,7 @@ func (paymentRepo *RepositoryDB) RefundUpdateAccount(amount float64, id string, 
 }
 
 func (paymentRepo *RepositoryDB) SaveRefundTracker(tracker *domain.RefundTracker) (*mongo.InsertOneResult, error) {
+	helpers.LogEvent("INFO", fmt.Sprintf("Saving refund tracker with  :%v", tracker))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("refund-tracker-collection")
 	result, err := collection.InsertOne(context.TODO(), tracker)
 	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
@@ -142,6 +151,7 @@ func (paymentRepo *RepositoryDB) SaveRefundTracker(tracker *domain.RefundTracker
 }
 
 func (paymentRepo *RepositoryDB) GetRefundTrackerByTransactionID(id string) (bson.M, error) {
+	helpers.LogEvent("INFO", fmt.Sprintf("Getting Refund tracker by transaction ID :%s", id))
 	collection := paymentRepo.db.Database("payment-gateway").Collection("refund-tracker-collection")
 	var result bson.M
 	err := collection.FindOne(
@@ -159,6 +169,8 @@ func (paymentRepo *RepositoryDB) GetRefundTrackerByTransactionID(id string) (bso
 }
 
 func (paymentRepo *RepositoryDB) VoidCard(id string, void bool) (interface{}, error) {
+
+	helpers.LogEvent("INFO", fmt.Sprintf("Void card with id of  :%s", id))
 
 	collection := paymentRepo.db.Database("payment-gateway").Collection("gateway")
 
@@ -180,13 +192,3 @@ func (paymentRepo *RepositoryDB) VoidCard(id string, void bool) (interface{}, er
 	}
 	return result.UpsertedID, err
 }
-
-// CheckIfPasswordExists   Check if the user password exists in the database
-
-// SaveTransaction Saving the payment-gateway transaction to the database
-
-// PostToAccount Posting the account details to the database
-
-// GetAccountBalance Getting account balance from the database
-
-// ChangeUserStatus Updating user status from the database
