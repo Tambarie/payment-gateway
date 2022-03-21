a40e64041dd811b8e73318a41d5b83d6d5cb01b2
tambarie <gt.gbaragbo@gmail.com>
2022-03-21 04:57:21 +0100

    connecting to atlas

--- infrastructure/repository/mongoDB/db.go
+++ infrastructure/repository/mongoDB/db.go
@@ -1,7 +1,6 @@
 package mongoDB
 
 import (
-	"fmt"
 	_ "github.com/lib/pq"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -14,18 +13,29 @@ import (
 // Init setting up mongDB
 func Init() *mongo.Client {
 
-	mongoURL := fmt.Sprintf("%s://%s:%s", os.Getenv("DB_TYPE"), os.Getenv("MONGO_DB_HOST"), os.Getenv("MONGO_DB_PORT"))
-
-	mongoTimeout := time.Minute * 15
-
-	// using go mongo-driver  to connect to mongoDB
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(mongoTimeout))
+	//mongoURL := fmt.Sprintf("%s://%s:%s", os.Getenv("DB_TYPE"), os.Getenv("MONGO_DB_HOST"), os.Getenv("MONGO_DB_PORT"))
+	//
+	//mongoTimeout := time.Minute * 15
+	//
+	//// using go mongo-driver  to connect to mongoDB
+	//ctx, cancel := context.WithTimeout(context.Background(), time.Duration(mongoTimeout))
+	//defer cancel()
+	//client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	//if err != nil {
+	//	log.Fatalf("error %v", err)
+	//}
+	//
+	//log.Println("Database Connected Successfully...")
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI(os.Getenv("MONGODB_URI")).
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		log.Fatalf("error %v", err)
+		log.Fatal(err)
 	}
-
 	log.Println("Database Connected Successfully...")
 	return client
 }
