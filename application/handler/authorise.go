35f868b1c50045ff0655db51fbadc8e92393027c
tambarie <gt.gbaragbo@gmail.com>
2022-03-21 04:56:21 +0100

    add comments

--- application/handler/authorise.go
+++ application/handler/authorise.go
@@ -10,15 +10,19 @@ import (
 	"net/http"
 )
 
+// Authorize handler authorizes the merchant to fund his wallet and gives him a unique ID to capture and refund payments
 func (h *Handler) Authorize() gin.HandlerFunc {
 	return func(context *gin.Context) {
+		// Logging the context
 		helpers.LogRequest(context)
 		userReference := context.Param("user-reference")
 
 		var merchant = &domain.Card{}
+
 		merchant.ID = uuid.New().String()
 		merchant.UserReference = userReference
 
+		// Binding the merchant json
 		if err := helpers.Decode(context, &merchant); err != nil {
 			log.Fatalf("Error %v", err)
 			return
@@ -31,24 +35,27 @@ func (h *Handler) Authorize() gin.HandlerFunc {
 			return
 		}
 
-		// check if credit card is valid
+		// checking the validation of the credit card using Luhn algorithm
 		if false == helpers.Valid(merchant.CardNumber) {
 			response.JSON(context, http.StatusBadRequest, nil, nil, "your credit card is not valid")
 			return
 		}
 
 		checker := 119
+		// Checking for the authorisation of the credit card
 		res := helpers.AuthorisationFailure(merchant.CardNumber, checker)
 		if res == true {
 			response.JSON(context, http.StatusBadRequest, nil, nil, "Authorisation failure")
 			return
 		}
 
+		// method that handles the authorization of the credit card
 		card, err := h.PaymentGatewayService.Authorise(merchant)
 		if err != nil {
 			log.Fatalf("Error %v", err)
 		}
 
+		//JSON response to the client
 		response.JSON(context, http.StatusCreated, gin.H{
 			"Unique ID": card.ID,
 			"Amount":    card.Amount,
